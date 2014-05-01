b2db44de7948f4a5e5ad8ff4ef1c0bf476c94f63
iOS <ios@Macbook06.local>
2014-04-30 22:07:54 -0500

    MOD1-Clase 1
    . Introduccion
    . Storyboard
    . Escenas, Vistas, Navegacion, Transiciones

---
+++ module_1/class_1/MiPrimeraApp/MiPrimeraApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MiPrimeraApp
+//
+//  Created by iOS on 4/30/14.
+//  Copyright (c) 2014 Area51. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ module_1/class_1/MiPrimeraApp/MiPrimeraApp/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  MiPrimeraApp
+//
+//  Created by iOS on 4/30/14.
+//  Copyright (c) 2014 Area51. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)elSwitchCambio:(UISwitch *)sender {
+    
+    self.view.backgroundColor = [UIColor greenColor];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
