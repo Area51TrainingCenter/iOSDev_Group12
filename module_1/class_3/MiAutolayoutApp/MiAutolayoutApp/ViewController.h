cc55e6f2c1e304f564df42eb8038df6a41064e3e
iOS <ios@Macbook06.local>
2014-05-05 21:56:42 -0500

    MOD1 Clase 3
    . Autolayout, parte 1

---
+++ module_1/class_3/MiAutolayoutApp/MiAutolayoutApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MiAutolayoutApp
+//
+//  Created by iOS on 5/5/14.
+//  Copyright (c) 2014 Area51. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ module_1/class_3/MiAutolayoutApp/MiAutolayoutAppTests/MiAutolayoutAppTests.m
@@ -0,0 +1,34 @@
+//
+//  MiAutolayoutAppTests.m
+//  MiAutolayoutAppTests
+//
+//  Created by iOS on 5/5/14.
+//  Copyright (c) 2014 Area51. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiAutolayoutAppTests : XCTestCase
+
+@end
+
+@implementation MiAutolayoutAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
