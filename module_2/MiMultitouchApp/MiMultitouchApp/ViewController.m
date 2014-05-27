7f19dd7a01f6355fa0c4b7142fba2869ee3eecfd
iOS <ios@Macbook06-2.local>
2014-05-26 21:55:58 -0500

    MOD2 Clase 4
    . Multitouch Parte 2: Transform, Rotate, Scale, LongPress

--- module_2/MiMultitouchApp/MiMultitouchApp/ViewController.m
+++ module_2/MiMultitouchApp/MiMultitouchApp/ViewController.m
@@ -35,8 +35,16 @@
     
 }
 
+- (IBAction)aparecerMenu:(UIButton *)sender {
+    
+    UIActionSheet *miMenu = [[UIActionSheet alloc] initWithTitle:nil delegate:nil cancelButtonTitle:@"Cancelar" destructiveButtonTitle:@"Eliminar" otherButtonTitles:@"Guardar a carrete",@"Denunciar", nil];
+    
+    [miMenu showInView:self.view];
+}
+
 
 - (IBAction)seHizoDobleTap:(UITapGestureRecognizer *)sender {
+    
     
     sender.view.backgroundColor = coloresOriginales[sender.view.tag];
     
@@ -55,28 +63,51 @@
     
     
     //2. Aplicarselo a la vista
-    
     CALayer *miLayer = sender.view.layer;
-    miLayer.frame;
     
+    //2a
+    //miLayer.position = punto;
     
-    CGPoint layerPunto =miLayer.position;
-    layerPunto.x = 8;
-    miLayer.position = layerPunto;
-    
-    
-    int valor=6;
-    int copia;
-    
-    copia = valor;
-    copia = 3;
-    valor = copia;
-    
-    
+    /*
+    //2b
+    CGPoint nuevaPosicion = miLayer.position;
+    nuevaPosicion.x += traslacion.x;
+    nuevaPosicion.y += traslacion.y;
+    miLayer.position = nuevaPosicion;
     
     [sender setTranslation:CGPointZero inView:self.view];
+    */
+     
+    //3
+    CATransform3D nuevoTransform = sender.view.layer.transform;
+    nuevoTransform = CATransform3DTranslate(nuevoTransform,traslacion.x, traslacion.y, 0);
+    sender.view.layer.transform = nuevoTransform;
     
+    [sender setTranslation:CGPointZero inView:self.view];
 }
+
+
+- (IBAction)seEstaHaciendoPinch:(UIPinchGestureRecognizer *)sender {
+    
+    /*
+    CGRect nuevoFrame = sender.view.layer.frame;
+    nuevoFrame.size.width *= sender.scale;
+    nuevoFrame.size.height *= sender.scale;
+    sender.view.layer.frame = nuevoFrame;
+   
+    sender.scale = 1;
+     */
+    
+    CATransform3D nuevoTransform = sender.view.layer.transform;
+    nuevoTransform = CATransform3DScale(nuevoTransform, sender.scale, sender.scale, 1);
+    sender.view.layer.transform = nuevoTransform;
+    
+    sender.scale = 1;
+
+}
+
+
+
 
 
 
