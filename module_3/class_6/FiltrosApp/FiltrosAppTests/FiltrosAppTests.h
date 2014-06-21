9bd7b86cba64f97248dde49e55805423b34c7ad0
Diego Cruz <diegocruz@MacBook-de-Diego.local>
2014-06-21 00:03:29 -0500

    App de Filtros

---
+++ module_3/class_6/FiltrosApp/FiltrosAppTests/FiltrosAppTests.h
@@ -0,0 +1,13 @@
+//
+//  FiltrosAppTests.h
+//  FiltrosAppTests
+//
+//  Created by iOS on 9/09/13.
+//  Copyright (c) 2013 Area51. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FiltrosAppTests : SenTestCase
+
+@end
---
+++ module_3/class_6/FiltrosApp/FiltrosAppTests/FiltrosAppTests.m
@@ -0,0 +1,32 @@
+//
+//  FiltrosAppTests.m
+//  FiltrosAppTests
+//
+//  Created by iOS on 9/09/13.
+//  Copyright (c) 2013 Area51. All rights reserved.
+//
+
+#import "FiltrosAppTests.h"
+
+@implementation FiltrosAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FiltrosAppTests");
+}
+
+@end
